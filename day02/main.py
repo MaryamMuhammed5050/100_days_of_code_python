454046a1b5accbf573665660c71a04003cf41549
MaryamMuhammed5050 <maryammoh2050@gmail.com>
2026-09-08 21:15:15 +0100

    day1 challenge

--- day02/main.py
+++ day02/main.py
@@ -1,8 +1,8 @@
 print("Welcome to the tip calculator!")
 bill = float(input("what is the total bill amount?\n$:"))
 tip = int(input("How much tip would you like to give?\npercent:"))
-tip1 = int(input("How many people to split the bill?\npeople:"))
+split = int(input("How many people to split the bill?\npeople:"))
 
-total = ("{:.2f}".format((((bill * (tip/100)) + bill) / tip1)))
+total = ("{:.2f}".format((((bill * (tip/100)) + bill) /  split)))
 
 print(f"Each person should pay: ${total}") 
